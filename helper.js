44c80c0e2106518c2efbc98326e270f8fd3e0b88
DynomiteWaffle <dynomitewaffle@protonmail.com>
2024-11-03 13:11:50 -0700

    QOL undo runs + detailed logs

--- helper.js
+++ helper.js
@@ -1,4 +1,6 @@
 let log = [0]
+let numbers = []
+let numberLog = [0]
 let run = 0
 // imports game function
 function getRules() {
@@ -7,6 +9,7 @@ function getRules() {
     init() //initilize new rules
     document.getElementById("start").hidden = true
     document.getElementById("game").hidden = false
+    updateVisuals()
 }
 
 
@@ -15,39 +18,52 @@ function genButtons(functions) {
     let loop = 0
     functions.forEach(element => {
         loop++
-        let button = document.createElement("button")
-        button.innerText = loop
-        button.addEventListener('click', () => {
+        const n = loop
+        let b = document.createElement("button")
+        b.innerText = loop
+        b.addEventListener('click', () => {
             element();
+            logDetailedLog(n+" button pressed, score "+log[log.length-1])
         });
-        document.getElementById("buttons").appendChild(button)
+        document.getElementById("buttons").appendChild(b)
     });
 
 }
 
 function undo() {
-    log.pop()
-    log[0] = 0 // start of log always 0
-    updateVisualScore()
+    let p
+    // remove 1 if 1 can be removed
+    if (log.length > 1) {
+        p = log.pop()
+        logDetailedLog("Undo, now run "+(run)+" score "+log[log.length-1])
+    }
+    if (p == 0 && log.length >0) { run-- }//undo a run
+    updateVisuals()
+
 }
 function newRun() {
+    logDetailedLog("New run, now run "+(run+2))
     run++
-    log = [0]
-    updateVisualScore()
+    log[log.length] = 0
+    updateVisuals()
 }
 
 function setScore(score) {
-    // console.log(score)
-    log[log.length + 1] = score
-    // update html info
-    // console.log(log[log.length])
-    updateVisualScore()
+    log[log.length] = score
+    updateVisuals()// update html info
 }
 function score() {
     return log[log.length-1]
 }
 
-function updateVisualScore() {
+function updateVisuals() {
     document.getElementById("score").innerText = log[log.length - 1]
-    document.getElementById("run").innerText = run
+    document.getElementById("run").innerText = run + 1
 }
+
+//Detailed Log
+function logDetailedLog(message) {
+    let log = document.getElementById("log").innerText
+    document.getElementById("log").innerText = message +"\n"+log
+}
+
